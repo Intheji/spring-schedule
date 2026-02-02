8401a75c8b74a2372a6d540c96abd0e78f7c5eb2
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-02 18:50:19 +0900

    feat: Schedule 엔티티 추가

---
+++ src/main/java/com/springschedule/entity/Schedule.java
@@ -0,0 +1,36 @@
+package com.springschedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "schedules")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(nullable = false)
+    private String authorName;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Schedule(String title, String content, String authorName, String password) {
+        this.title = title;
+        this.content = content;
+        this.authorName = authorName;
+        this.password = password;
+    }
+}
