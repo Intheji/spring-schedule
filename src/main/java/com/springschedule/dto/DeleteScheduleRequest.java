599a503703c7a7b356a51337f8f493384eb70c2e
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-03 14:14:46 +0900

    feat: 일정 삭제 요청 dto 추가

---
+++ src/main/java/com/springschedule/dto/DeleteScheduleRequest.java
@@ -0,0 +1,10 @@
+package com.springschedule.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteScheduleRequest {
+    private String password;
+}
