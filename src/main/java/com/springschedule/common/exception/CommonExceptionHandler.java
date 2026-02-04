a80c5e84858e71ed032cf7fb27a2242f4d61f502
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-04 13:08:20 +0900

    feat: 예외 처리 400 응답 추가

---
+++ src/main/java/com/springschedule/common/exception/CommonExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.springschedule.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+}
